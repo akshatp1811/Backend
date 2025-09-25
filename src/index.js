7f532e612ef3ec33261ebaf34162130a90a79d4c
akshatp1811 <akshatpanday28@gmail.com>
2025-09-25 12:23:55 +0530

    dev commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "../db";
+
+connectDB()
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+
+
+/*
+CONNECTING DB THROUGH METHOD 1
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+
+const app = express()
+(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}`/`${DB_NAME}`)
+
+    // Handle server errors
+    app.on("error", (error) => {
+      console.log("ERRR: ", error);
+      throw error;  
+    });
+
+    // Start the server
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+    }catch(error){
+        console.error("ERROR: ",error);
+        throw err
+    }
+})()
+    */
